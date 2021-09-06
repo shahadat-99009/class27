71dc3ed0a75463b29de419468d2e686607635579
shahadat-99009 <shahadathossain999000@gmail.com>
2021-09-06 12:59:34 -0700

    file created

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+
+ $(function(){
+//    slick slider 
+$('.banner_slider').slick({
+  
+    arrows:true,
+    prevArrow:' <i class="slick-prev fas fa-chevron-left prev_arrow"></i>',
+    nextArrow:'<i class="slick-prev fas fa-chevron-right next_arrow"></i>',
+    dots:true,
+    autoplay:true,
+      });
+
+      $('.venobox').venobox({
+        closeBackground:'red',
+        spinner:'wave',
+       });
+     
+     
+ });
+
+
